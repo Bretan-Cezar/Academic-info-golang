de59fc647ce18c045cd18bf01705be7ef1e4b864
Bretan-Cezar <39191865+Bretan-Cezar@users.noreply.github.com>
2022-05-05 04:28:47 +0300

    View grades & Manage optionals fully implemented, an issue with propose optionals to be fixed

--- react-app/src/student/Student.js
+++ react-app/src/student/Student.js
@@ -5,6 +5,7 @@ import ViewCurriculum from "../view_curriculum/ViewCurriculum";
 import ViewProfile from "../view_profile/ViewProfile";
 import ModifyProfile from "../modify_profile/ModifyProfile";
 import ViewOptionals from "../view_optionals/ViewOptionals";
+import ViewGrades from "../view_grades/ViewGrades";
 
 function StudentFeatureSelector({ feature_name }) {
 	switch (feature_name) {
@@ -22,6 +23,10 @@ function StudentFeatureSelector({ feature_name }) {
 
 		case "view_optionals": {
 			return <ViewOptionals />;
+		}
+
+		case "view_grades": {
+			return <ViewGrades />;
 		}
 		default: {
 		}
@@ -56,7 +61,7 @@ function Student({ setUserData }) {
 					<div className="Sidebar-item">
 						<p>View Contract</p>
 					</div>
-					<div className="Sidebar-item">
+					<div className="Sidebar-item" onClick={() => setFeature("view_grades")}>
 						<p>View Grades</p>
 					</div>
 					<div className="Sidebar-logout-item" onClick={() => setUserData("")}>
---
+++ react-app/src/view_grades/ViewGrades.js
@@ -0,0 +1,104 @@
+import React from "react";
+import "./ViewGrades.css";
+import { UserContext } from "../App";
+
+function GradesList({ grades_array }) {
+	return (
+		<>
+			{grades_array.map((grade) => (
+				<tr>
+					<td className="Grades-table-body-item">{grade.disciplineName}</td>
+					<td className="Grades-table-body-item">{grade.creditCount}</td>
+					<td className="Grades-table-body-item">{grade.value}</td>
+				</tr>
+			))}
+		</>
+	);
+}
+
+function ViewGrades() {
+	const userData = React.useContext(UserContext);
+
+	let [yearsList, setYearsList] = React.useState([]);
+	let [selectedYear, setSelectedYear] = React.useState(-1);
+	let [gradesList, setGradesList] = React.useState([]);
+
+	let yearUrl = "http://localhost:1337/http://localhost:8090/student/getFacultiesAndSpecializations/" + userData.username;
+
+	React.useEffect(() => {
+		const yearRequestOptions = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				"Access-Control-Allow-Origin": "origin",
+				Authorization: "Bearer " + userData.auth_token,
+			},
+		};
+
+		const yearRequest = new Request(yearUrl, yearRequestOptions);
+
+		fetch(yearRequest)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setYearsList(data);
+			});
+
+		if (selectedYear != -1) {
+			let gradesUrl = "http://localhost:1337/http://localhost:8090/student/getGrades/" + userData.username + "/" + selectedYear;
+
+			const gradesRequestOptions = {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					"Access-Control-Allow-Origin": "origin",
+					Authorization: "Bearer " + userData.auth_token,
+				},
+			};
+
+			const gradesRequest = new Request(gradesUrl, gradesRequestOptions);
+
+			fetch(gradesRequest)
+				.then((response) => response.json())
+				.then((data) => {
+					console.log(data);
+					setGradesList(data);
+				});
+		}
+	}, [userData, selectedYear]);
+
+	return (
+		<>
+			<select
+				className="Year-selector"
+				onChange={(e) => {
+					setSelectedYear(e.target.value);
+				}}
+				value={selectedYear}
+			>
+				<option value="-1">--Select--</option>
+				{yearsList.map((y) => {
+					return (
+						<option key={y.yosId} value={y.yosId}>
+							{y.yosId + " - " + y.facultyName + ", specialization " + y.specialization + " - year " + y.yearNo}
+						</option>
+					);
+				})}
+			</select>
+			<table cellSpacing="0" cellPadding="12" className="Grades-table">
+				<thead>
+					<tr>
+						<td className="Grades-table-head-item">Name</td>
+						<td className="Grades-table-head-item">Credits</td>
+						<td className="Grades-table-head-item">Value</td>
+					</tr>
+				</thead>
+				<tbody>
+					<GradesList grades_array={gradesList} />
+				</tbody>
+			</table>
+		</>
+	);
+}
+
+export default ViewGrades;
